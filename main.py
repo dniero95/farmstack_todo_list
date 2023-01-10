0680106dbeb2fb08f24fbf674e65ce85763d9121
dniero95 <darioniero@icloud.com>
2023-01-10 16:00:01 +0100

    add endpoints

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from model import Todo
+
 # create app obj
 
 app = FastAPI()
@@ -18,3 +20,16 @@ app.add_middleware(
 @app.get('/')
 def read_root():
     return {'message': 'welcome from ToDo list'}
+
+@app.get('/todo/{id}')
+async def get_todo_by_id(id):
+    pass
+@app.post('/add/todo')
+async def add_todo(todo:Todo):
+    pass
+@app.put('/update/todo/{id}')
+async def update_todo(id, data):
+    pass
+@app.delete('/delete/todo/{id}')
+async def delete_todos(id):
+    pass
